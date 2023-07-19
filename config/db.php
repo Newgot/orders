ce691d7be572aac1891c7481f0eb5742b0ccffdf
newgot <newgot@mail.ru>
2023-07-19 08:58:29 +0300

    fix db config

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=yii_db',
+    'dsn' => 'mysql:host=db;dbname=yii_db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
