52ad99b2c20bf11ee9fc77341f945ba41ca98e86
newgot <newgot@mail.ru>
2023-07-27 10:10:40 +0300

    db connection fix

--- app/config/db.php
+++ app/config/db.php
@@ -2,9 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname='. $_ENV['DB_NAME'],
+    'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname='. $_ENV['DB_NAME'],
     'username' => $_ENV['DB_USER'],
     'password' => $_ENV['DB_PASS'],
     'charset' => 'utf8',
-
 ];
